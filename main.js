c2ba687a14ebddc9b660d8ec8d9b5a3feee91f48
Ahnafsaihaan <96058254+Ahnafsaihaan@users.noreply.github.com>
2023-05-20 19:01:03 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 img = "";
+status = "";
+objects = [];
 
 function preload()
 {
@@ -8,19 +10,43 @@ function setup()
 {
     canvas = createCanvas(640, 420);
     canvas.center();
+    objectDetector = ml5.objectDetector("cocoSsd", modelLoaded);
+    document.getElementById("status").innerHTML = "Status = Detecting Objects";
+}
+function modelLoaded()
+{
+    console.log("ModelLoaded !");
+    status = true;
+    objectDetector.detect(img, gotResult);
+}
+function gotResult(error, results)
+{
+    if (error) 
+    {
+        console.error();
+    }
+    else
+    {
+        console.log(results);
+        objects = results ;
+    }
 }
 function draw()
 {
     image(img, 0, 0, 640, 420);
-    fill("#FF0000");
-    text("dog", 45, 75);
-    noFill();
-    stroke("#000000");
-    rect(30, 60, 450, 350);
 
-    fill("#FF0000");
-    text("cat" , 320, 120);
-    noFill();
-    stroke("#000000");
-    rect(300, 90, 280, 320);
+    if(status != "")
+    {
+        for(i=0; i < objects.length ; i++)
+        {
+            document.getElementById("status").innerHTML = "Status = Object Detected";
+
+            fill("#FF0000");
+            noFill();
+            percent = floor(objects[i].confidence * 100) ;
+            text(objects[i].label + " " + percent + "%",  objects[i].x + 15,  objects[i].y + 15);
+            stroke("#FF0000");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+    }
 }
